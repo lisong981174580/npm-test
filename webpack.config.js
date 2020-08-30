75828f3fbee1b75cb536753eb16c0bdf2f05cdef
李松 <allen@lisongdeMacBook-Pro-2.local>
2020-08-30 11:45:41 +0800

    feat: 基本配置完成

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', // production 模式会被压缩
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    })
+  ],
+  devServer: {
+    port: 3000,
+    contentBase: path.join(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+}
